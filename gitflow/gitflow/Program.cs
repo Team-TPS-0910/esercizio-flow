3074fec732cb9b734a2accfb54f345fd3c163993
PIETRO SPREAFICO 5IA <pietro.spreafico@issgreppi.it>
2020-10-12 10:36:11 +0200

    commit flow

---
+++ gitflow/gitflow/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace gitflow
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("aggiungete una classe col vostro nome");
+        }
+    }
+}
